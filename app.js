3e76979df7b27b64a027b765fb8c1ef95a3b876d
Daniel Huang <dah0417912@gmail.com>
2018-02-09 05:59:13 +0000

    fbgraph module added

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var expressSanitizer= require("express-sanitizer"),
     bodyParser      = require("body-parser"),
     mongoose        = require("mongoose"),
     express         = require("express"),
-    app             = express();
+    app             = express(),
+    graph   = require('fbgraph');
     
 //APP CONFIG
 //mongoose.connect("mongodb://localhost/restful_blog_app");  
@@ -122,6 +123,60 @@ app.delete("/blogs/:id", function(req,res){
    });
    //redirect to index page
 });
+
+// this should really be in a config file!
+var conf = {
+    client_id:      '200133944056296'
+  , client_secret:  '1366d88c90d9e70c8f697207c728491b'
+  , scope:          'email, user_about_me, user_birthday, user_location, publish_actions'
+  // You have to set http://localhost:3000/ as your website
+  // using Settings -> Add platform -> Website
+  , redirect_uri:   'https://chatbot-site.herokuapp.com/blogs'
+};
+app.get('/auth', function(req, res) {
+
+  // we don't have a code yet
+  // so we'll redirect to the oauth dialog
+  if (!req.query.code) {
+    console.log("Performing oauth for some user right now.");
+  
+    var authUrl = graph.getOauthUrl({
+        "client_id":     conf.client_id
+      , "redirect_uri":  conf.redirect_uri
+      , "scope":         conf.scope
+    });
+
+    if (!req.query.error) { //checks whether a user denied the app facebook login/permissions
+      res.redirect(authUrl);
+    } else {  //req.query.error == 'access_denied'
+      res.send('access denied');
+    }
+  }
+  // If this branch executes user is already being redirected back with 
+  // code (whatever that is)
+  else {
+    console.log("Oauth successful, the code (whatever it is) is: ", req.query.code);
+    // code is set
+    // we'll send that and get the access token
+    graph.authorize({
+        "client_id":      conf.client_id
+      , "redirect_uri":   conf.redirect_uri
+      , "client_secret":  conf.client_secret
+      , "code":           req.query.code
+    }, function (err, facebookRes) {
+      res.redirect('/UserHasLoggedIn');
+    });
+  }
+});
+
+
+// user gets sent here after being authorized
+app.get('/UserHasLoggedIn', function(req, res) {
+  res.render("index", { 
+      title: "Logged In" 
+  });
+});
+
 app.listen(process.env.PORT, process.env.IP,function(){
     
     console.log("Server is running");
